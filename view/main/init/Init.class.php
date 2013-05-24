ea55e1e297d33a41be2252fe3d363b67715a1d2e
Tadashi Nagao <tadashi.nagao@gmail.com>
2013-05-24 22:39:08 +0900

    modified:   view/main/init/Init.class.php

--- view/main/init/Init.class.php
+++ view/main/init/Init.class.php
@@ -30,11 +30,15 @@ class Patchworks_View_Main_Init extends Action
     {
         $this->patchworks_id=$this->patchworksView->getPatchworksID($this->block_id);
 
+
         $this->handle = $this->session->getParameter('_handle');
 
         if (  $this->patchworks_id < 1 ){
           return 'success';
         }
+// ここでコードを読み込む
+       include(BASE_DIR .'/webapp/modules/patchworks/patchs/1.php');
+
          return  'success' . $this->patchworks_id; 
     }
 }
