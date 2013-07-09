3a1864e1a318080aec7b069a3a75ee8cf5867397
tadashi nagao <tadashi.nagao@gmail.com>
2013-07-09 17:43:30 +0900

    コピーライト削除

--- view/edit/base/Base.class.php
+++ view/edit/base/Base.class.php
@@ -1,17 +1,5 @@
 <?php
 
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
-/**
- * patchworks 
- *
- * @package     NetCommons
- * @author      Tadashi Nagao etc
- * @copyright   2013-2013 Patchworks for N7etCommons
- * @license     http://www.netcommons.org/license.txt  NetCommons License
- * @project     Patchworks for NetCommons  
- * @access      public
- */
 class Patchworks_View_Edit_Base extends Action
 {
     // パラメータを受け取るため
--- view/main/init/Init.class.php
+++ view/main/init/Init.class.php
@@ -1,17 +1,5 @@
 <?php
 
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
-/**
-* [[機能説明]]
- *
- * @package     NetCommons
- * @author      Noriko Arai,Ryuji Masukawa
- * @copyright   2006-2007 NetCommons Project
- * @license     http://www.netcommons.org/license.txt  NetCommons License
- * @project     NetCommons Project, supported by National Institute of Informatics
- * @access      public
- */
 class Patchworks_View_Main_Init extends Action
 {
     /**
