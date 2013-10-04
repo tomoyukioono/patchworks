1b6834d96805c3f0d06678f652d43b4eb614a9ff
tadashi nagao <tadashi.nagao@gmail.com>
2013-10-04 16:10:49 +0900

    modified:   templates/default/patchworks_view_edit_base.html 	modified:   templates/default/patchworks_view_main_init_default.html 	modified:   view/edit/base/Base.class.php 	modified:   view/main/init/Init.class.php

--- view/edit/base/Base.class.php
+++ view/edit/base/Base.class.php
@@ -11,8 +11,6 @@ class Patchworks_View_Edit_Base extends Action
     var $request = null;
     var $session = null;
 
-
-
     /**
      * 工事中
      *
@@ -20,19 +18,23 @@ class Patchworks_View_Edit_Base extends Action
      */
     function execute()
     {
+     // モジュール全体の設定情報を保存する。仕組みとしては、patchwroks の
+     // 999999 番に保存する。
      $this->patchworks_id=999999;
      $this->config=$this->patchworksView->getConfig($this->patchworks_id);
+     // pattchworks の一覧を出す。extra/addin/patchworksID に、patchworks は
+     // 配置されている
      $x = scandir(BASE_DIR . "/extra/addin/patchworksID");
      $xarray = array();
-     // 一覧に 999999 はでてこない
+     // patchwroks 一覧に 999999 はでてこない
      foreach ($x as $k=>$v) {
-       if (intval($v) >0 && intval($v) <> 999999) {
+       if (intval($v) > 0 && intval($v) <> 999999) {
          $xarray[]=$v;
        }  
      }
      sort($xarray); 
 
-     //$this->xxx = print_r($xarray,true);
+     // patchworksID　のうち一覧に出すものを選択する 
      $this->config['patchworks_all'] = implode(',',$xarray); 
      if (isset($this->config['patchworks_id'])) 
          {$this->patchworks_data_flag = 1 ;}
@@ -40,9 +42,9 @@ class Patchworks_View_Edit_Base extends Action
                 return "error";
             }
 
-
-$sql = "select patchworks_id as id ,count(*) as count from nc_patchworks ".
-        " group by patchworks_id order by patchworks_id; ";
+        $sql = "select patchworks_id as id ,count(*) as count "
+               . "from {patchworks} "
+               . " group by patchworks_id order by patchworks_id; ";
         $params=array();
         $this->patchworks = $this->patchworksView->getDataBySql($params,$sql);
         return "base";
